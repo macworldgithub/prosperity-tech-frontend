dc1e7abb0fb9b363be17ebe484d8aa9da30cb023
Hammad Lodhi <hammadak05@gmail.com>
2025-12-02 12:08:26 +0500

    prepaid and postpaid done

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const formatDob = (value: string) => {
+  if (!value) return "";
+  const [year, month, day] = value.split("-");
+  return `${day}/${month}/${year}`;
+};
