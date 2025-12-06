5910d38733248dcbc045316a139bfa368b90ee6f
hammadlodhi <hammadak05@gmail.com>
2025-12-06 17:20:05 +0500

    date format fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const formatDob = (value: string) => {
-  if (!value) return "";
-  const [year, month, day] = value.split("-");
-  return `${day}/${month}/${year}`;
+export const formatDob = (isoDate:any) => {
+  if (!isoDate) return "";
+  const [year, month, day] = isoDate.split("-");
+  return `${day}/${month}/${year}`;   // e.g. 15/03/1990
 };
